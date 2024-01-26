040ec7652872c2378814b27c1f943e727a55c1b1
Erick Pfeifer <erick.pfeifer@gmail.com>
2024-01-26 08:00:57 -0600

    Fixed BUILD files to adjust for latest Pigweed updates on Bazel

--- targets/tools/flash.py
+++ targets/tools/flash.py
@@ -1,4 +1,4 @@
-"""Script program target over openocd"""
+"""Script program target over openocd."""
 
 import subprocess
 
@@ -9,7 +9,7 @@ _BINARY_PATH = "demo_uart/app.elf"
 _OPENOCD_PATH = "openocd/bin/openocd"
 _OPENOCD_CONFIG_PATH = "openocd/scripts/board/microchip_same54_xplained_pro.cfg"
 
-# Vendor ID and model ID.
+# Vendor ID and model ID for SAME54 XplainedPRO.
 _VENDOR_ID = 0x03eb
 _PRODUCT_ID = 0x2111
 
@@ -28,13 +28,13 @@ def flash(board_serial):
     binary = r.Rlocation(_BINARY_PATH)
     openocd_cfg = r.Rlocation(_OPENOCD_CONFIG_PATH)
 
+    print(f"Board serial port signature: {board_serial}")
     print(f"binary Rlocation is: {binary}")
     print(f"openocd Rlocation is: {openocd}")
     print(f"openocd config Rlocation is: {openocd_cfg}")
 
     assert binary is not None
     assert openocd_cfg is not None
-    # " \"program build/E54ServoChar.elf verify\" -c \"reset halt\" -c shutdown"
     subprocess.check_call(
         [
             openocd,
@@ -42,10 +42,6 @@ def flash(board_serial):
             f"{openocd_cfg}",
             "-c",
             f"program {binary} reset exit",
-            # "-c",
-            # "adapter speed 4000",
-            # "-c",
-            # "bindto 0.0.0.0",
         ], )
 
 
